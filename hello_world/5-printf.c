cb37afafb7a9370c57086ad38d7773ef07da4f21
Your Name <you@example.com>
2024-10-21 19:23:38 +0200

    Betty

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -2,13 +2,11 @@
 /**
  * main - Entry point
  *
- * Description: Prints "Programming is like building a multilingual puzzle"
- * using the puts function, followed by a new line.
- *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
+
