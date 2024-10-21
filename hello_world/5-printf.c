fe571a55f1f30ec571ad28d0e0b46129dd713526
Your Name <you@example.com>
2024-10-21 17:28:06 +0200

    Betty

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,2 +1,14 @@
-#!/bin/bash
-
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Prints "Programming is like building a multilingual puzzle"
+ * using the puts function, followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
