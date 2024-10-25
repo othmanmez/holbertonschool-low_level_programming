53dbf4b30e93d02bc103c8c606710cac1c83201b
Your Name <you@example.com>
2024-10-25 12:28:33 +0200

    BEtty3

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,10 +1,11 @@
+#include "main.h"
 #include <stdio.h>
 /**
  * main - Affiche l'alphabet en minuscules dix fois.
  *
  * Return: Toujours 0.
  */
-int main(void)
+void print_alphabet_x10(void)
 {
 	int i, j;
 
@@ -16,5 +17,4 @@ int main(void)
 		}
 		putchar('\n');
 	}
-	return (0);
 }
