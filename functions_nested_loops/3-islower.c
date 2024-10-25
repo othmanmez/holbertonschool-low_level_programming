83bcc9cfef51131126fa8346071dcbb901fe4b99
Your Name <you@example.com>
2024-10-25 14:59:11 +0200

    Betty3

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
+/**
+ * _islower - checks if a character is lowercase
+ * @c: the character to check
+ *
+ * Return: 1 if c is lowercase, 0 otherwise
+ */
+
 int _islower(int c)
 {
 	/* Check if c is in the range of lowercase letters 'a' to 'z'*/
