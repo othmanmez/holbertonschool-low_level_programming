efe39e6c97eb78fa72dfd3c0b48e7c6d6a74534d
Your Name <you@example.com>
2024-10-25 11:25:13 +0200

    betty3

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -8,8 +8,9 @@
  */
 int _isalpha(int c)
 {
-if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-{
- ( return 1;);
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	return (0);
 }
- ( return 0;);
