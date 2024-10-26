391bfb16911be79612ef81b6630e9744f76b0108
Your Name <you@example.com>
2024-10-27 00:59:57 +0200

    Betty3

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,16 +1,18 @@
-#include "main.c"
-
+#include "main.h"
 /**
- * jack_bauer - prints every minute of the day of Jack Bauer
+ * print_last_digit - prints the last digit of a number
+ * @n: the integer from which to extract the last digit
  *
- * This function prints the time from 00:00 to 23:59,
- * one minute at a time.
+ * Return: the value of the last digit
  */
-void jack_bauer(void)
+int print_last_digit(int n)
 {
-    int hour, minute;
-
-    for (hour = 0; hour < 24; hour++)
-    {
-        for (minute = 0;
-
+	int last_digit;
+        	/* Get the absolute value of the last digit */
+        	last_digit = n % 10;
+			if (last_digit < 0)
+                		last_digit *= -1;
+        			/* Print the last digit */
+        				_putchar('0' + last_digit);
+        					return (last_digit);
+}
