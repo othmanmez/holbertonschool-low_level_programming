8f57ee05816539ba0c3ab87008e98247d987bb33
Your Name <you@example.com>
2024-10-26 13:59:40 +0200

    Betty3

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -17,7 +17,7 @@ int print_last_digit(int n)
 		last_digit = -last_digit;
 	}
 
-	putchar(last_digit + '0');
+	_putchar(last_digit + '0');
 
 	return (last_digit);
 }
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -10,5 +10,5 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 int _abs(int);
-void print_alphabet_x10(void);
+void print_alphabet(void);
 #endif
