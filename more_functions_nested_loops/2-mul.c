cbf4da1062894735a225471ab006a0f0ed96ad4a
Your Name <you@example.com>
2024-10-28 22:17:40 +0100

    Betty3

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - vérifie si un caractère est en majuscule
+ * @a: le premier entier
+ * @b: le deuxième entier
+ *
+ * Return: a et b
+ */
+
+int mul(int a, int b)
+
+{
+	{	return a * b;
+
+	}
+	printf("%d\n", mul(98, 1024));
+
+	return (0);
+
+
+}
