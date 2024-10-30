0335e530c4d4478b8f61c30782fc31756c3a098f
Your Name <you@example.com>
2024-10-30 15:20:02 +0100

    B4

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * swap_int - échange la valeur de 2 entier.
+ *
+ * @a: premier entier
+ * @b: deuxième entier
+ */
+
+void swap_int(int *a, int *b)
+{
+	int  temp;
+
+	temp = a;
+	a = b;
+	b = temp;
+}
