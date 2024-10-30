8cba67aeb37dc8c30571da55bf334927bc724def
Your Name <you@example.com>
2024-10-30 12:06:00 +0100

    B4

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * reset_to_98 - met à jour la valeur d'un entier à 98
+ * @n: pointeur vers l'entier à mettre à jour
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;  /* Déréférence le pointeur et définit la valeur à 98*/
+}
+
