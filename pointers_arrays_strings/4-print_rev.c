3da82b6c8e2fe90d2388bfb4555ce6948e65a276
Your Name <you@example.com>
2024-10-31 00:23:54 +0100

    B4

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -7,8 +7,15 @@ void print_rev(char *s)
 {
 	int len = 0;
 
-	/* Find the length of the string */
 	while (s[len] != '\0')
-
+	{
 		len++;
+	}
+
+	for (len -= 1; len >= 0; len--)
+	{
+		_putchar(s[len]);
+	}
+
+	_putchar('\n');
 }
