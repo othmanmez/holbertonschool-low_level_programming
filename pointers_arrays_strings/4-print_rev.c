a8caab511698c9f395650ad4924954b5b71b04ad
Your Name <you@example.com>
2024-10-30 22:00:06 +0100

    B4

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_rev - Prints a string in reverse, followed by a new line
+ * @s: Pointer to the string to be printed in reverse
+ */
+void print_rev(char *s)
+{
+	int len = 0;
+
+	/* Find the length of the string */
+	while (s[len] != '\0')
+
+		len++;
+}
