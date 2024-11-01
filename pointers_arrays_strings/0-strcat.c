d7ae15f9fc17c50b6dadfb5b0faa0385e318380a
Your Name <you@example.com>
2024-11-01 14:32:28 +0100

    B4

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -15,21 +15,12 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int i = 0;
-	int j = 0;
-	{
-		while (dest[i] != '\0')
-			i++;
-	}
-	{
-		while (src[j] != '\0')
+	int i, j;
 
-			dest[i + j] = src[j];
-		j++;
-	}
-
-	dest[i + j] = '\0';
-
+	for (i = 0; dest[i] != '\0'; i++)
+		;
+	for (j = 0; src[j] != '\0'; j++, i++)
+		dest[i] = src[j];
+	dest[i] = '\0';
 	return (dest);
 }
-
