d10f885aab9a35d56bc3fd81624cc5a3ec8662c2
Your Name <you@example.com>
2024-11-13 13:43:10 +0100

    B7

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+/**
+ * malloc_checked - allocate memory
+ * @b: number of bytes
+ * Return: pointer string
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *str;
+	str = malloc(b);
+	if (str == NULL)
+		exit(98);
+	return (str);
+}
