2396ad588369151c56427f49b7284875bf88dd54
Your Name <you@example.com>
2024-12-05 12:07:37 +0100

    singly

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "list.h"  // Inclure list.h
+#include "lists.h"
+/*
+ * add_node_end - Ajoute un nouveau noeud à la fin de la liste.
+ * @head: Pointeur vers l'adresse du premier élément de la liste.
+ * @str: Chaîne de caractères à ajouter dans le nouveau noeud.
+ *
+ * Description:
+ * Cette fonction ajoute un nouveau noeud à la fin de la liste simplement
+ * chaînée. Si la liste est vide, le nouveau noeud devient le premier
+ * élément de la liste.
+ *
+ * Retour:
+ * L'adresse du nouveau noeud si l'ajout réussit, sinon NULL si l'ajout
+ * échoue (par exemple, en cas d'échec d'allocation mémoire).
+ */
 
 list_t *add_node_end(list_t **head, const char *str)
 {
@@ -19,7 +33,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	new_node->str = strdup(str);
 	if (new_node->str == NULL)
 	{
-		free(new_node);
+		free(new_node);  /* Libération de la mémoire en cas d'échec de strdup */
 		return (NULL);
 	}
 
@@ -39,7 +53,7 @@ list_t *add_node_end(list_t **head, const char *str)
 		{
 			temp = temp->next;
 		}
-		temp->next = new_node;
+		temp->next = new_node;  /* Ajout du nouveau noeud à la fin */
 	}
 
 	return (new_node);  /* Retour de l'adresse du nouveau noeud */
