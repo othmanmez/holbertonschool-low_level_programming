fe9b7e0544130cfc7e690f082dcd223d9249dc95
Your Name <you@example.com>
2024-12-05 11:47:58 +0100

    singly

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,30 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-typedef struct list_s {
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+struct list_s
+{
 	char *str;
 	struct list_s *next;
-} list_t;
+};
 
-list_t *add_node(list_t **head, const char *str) {
+typedef struct list_s list_t;
+
+/**
+ * add_node - adds a new node at the beginning of a list_t list
+ * @head: double pointer to the head of the list
+ * @str: string to be added to the node
+ *
+ * Return: address of the new element, or NULL if it failed
+ */
+list_t *add_node(list_t **head, const char *str)
+
+{
 	list_t *new_node;
 
 	new_node = (list_t *)malloc(sizeof(list_t));
-	if (new_node == NULL) {
-		return NULL;
-	}
+	if (new_node == NULL)
+		return (NULL);
 
 	new_node->str = strdup(str);
-	if (new_node->str == NULL) {
+	if (new_node->str == NULL)
+	{
 		free(new_node);
-		return NULL;
+		return (NULL);
 	}
 
 	new_node->next = *head;
 
 	*head = new_node;
 
-	return new_node;
+	return (new_node);
 }
 
