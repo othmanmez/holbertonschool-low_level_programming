834e955256ed1252d7ad4555e81e405054b5d602
Your Name <you@example.com>
2024-12-06 17:33:25 +0100

    file

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -2,8 +2,6 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h>
-#include <sys/stat.h>
 
 /**
  * main - Copies content from one file to another
@@ -14,18 +12,15 @@
  */
 int main(int argc, char *argv[])
 {
-    int file_from, file_to, read_bytes, write_bytes;
+    int file_from, file_to, r, w;
     char buffer[1024];
-    struct stat st;
 
-    /* Check if the number of arguments is correct */
     if (argc != 3)
     {
         dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
         exit(97);
     }
 
-    /* Open the source file (file_from) */
     file_from = open(argv[1], O_RDONLY);
     if (file_from == -1)
     {
@@ -33,7 +28,6 @@ int main(int argc, char *argv[])
         exit(98);
     }
 
-    /* Check if the destination file (file_to) exists, and truncate it */
     file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
     if (file_to == -1)
     {
@@ -42,11 +36,10 @@ int main(int argc, char *argv[])
         exit(99);
     }
 
-    /* Copy the contents of file_from to file_to */
-    while ((read_bytes = read(file_from, buffer, sizeof(buffer))) > 0)
+    while ((r = read(file_from, buffer, 1024)) > 0)
     {
-        write_bytes = write(file_to, buffer, read_bytes);
-        if (write_bytes != read_bytes)
+        w = write(file_to, buffer, r);
+        if (w != r)
         {
             dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
             close(file_from);
@@ -55,8 +48,7 @@ int main(int argc, char *argv[])
         }
     }
 
-    /* Check if read() failed */
-    if (read_bytes == -1)
+    if (r == -1)
     {
         dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
         close(file_from);
@@ -64,14 +56,12 @@ int main(int argc, char *argv[])
         exit(98);
     }
 
-    /* Close the source file */
     if (close(file_from) == -1)
     {
         dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
         exit(100);
     }
 
-    /* Close the destination file */
     if (close(file_to) == -1)
     {
         dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
