21fb3ab6164b55f40cc18d2f6c02d8b2e29ad6c4
Your Name <you@example.com>
2024-12-06 12:13:40 +0100

    singly

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,35 +1,53 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "lists.h"
+
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for project
+ */
+typedef struct list_s
+{
+	char *str;             /* The string stored in the node */
+	struct list_s *next;   /* Pointer to the next node */
+} list_t;
 
 /**
  * add_node - adds a new node at the beginning of a list_t list
- * @head: double pointer to the head of the list
- * @str: string to be added to the node
+ * @head: pointer to the head of the list
+ * @str: string to be duplicated and stored in the new node
  *
- * Return: address of the new element, or NULL if it failed
+ * Return: the address of the new element, or NULL if it failed
  */
 list_t *add_node(list_t **head, const char *str)
-
 {
 	list_t *new_node;
 
-	new_node = (list_t *)malloc(sizeof(list_t));
+	/* Step 1: Allocate memory for the new node */
+	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
-		return (NULL);
+	{
+		return (NULL); /* Memory allocation failed */
+	}
 
+	/* Step 2: Duplicate the string */
 	new_node->str = strdup(str);
 	if (new_node->str == NULL)
 	{
-		free(new_node);
+		free(new_node); /* If strdup fails, free the new node and return NULL */
 		return (NULL);
 	}
 
-	new_node->next = *head;
+	/* Step 3: Insert the new node at the beginning of the list */
+	new_node->next = *head;  /* Point to the current head */
+	*head = new_node;        /* Update the head to point to the new node */
 
-	*head = new_node;
-
+	/* Step 4: Return the new node */
 	return (new_node);
 }
 
