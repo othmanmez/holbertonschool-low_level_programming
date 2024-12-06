2615f8b38f696b7c28e50239744d4841c48cb586
Your Name <you@example.com>
2024-12-06 13:38:47 +0100

    doubly

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -13,43 +13,36 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *new_node, *current;
 	unsigned int i;
 
-	/* Allocate memory for the new node */
 	new_node = malloc(sizeof(dlistint_t));
-	if (new_node == NULL)
+	if (!new_node)
 		return (NULL);
 
 	new_node->n = n;
 	current = *h;
 
-	/* Insert at the head (idx == 0) */
 	if (idx == 0)
 	{
 		new_node->next = *h;
 		new_node->prev = NULL;
-		if (*h != NULL)
+		if (*h)
 			(*h)->prev = new_node;
 		*h = new_node;
 		return (new_node);
 	}
 
-	/* Traverse the list to the correct position */
-	for (i = 0; current != NULL && i < idx - 1; i++)
+	for (i = 0; current && i < idx - 1; i++)
 		current = current->next;
 
-	/* If we reached the end of the list before the desired index, return NULL */
-	if (current == NULL)
+	if (!current)
 	{
 		free(new_node);
 		return (NULL);
 	}
 
-	/* Insert the new node at the correct position */
 	new_node->next = current->next;
 	new_node->prev = current;
-
-	if (current->next != NULL)
+	if (current->next)
 		current->next->prev = new_node;
-
 	current->next = new_node;
 
 	return (new_node);
