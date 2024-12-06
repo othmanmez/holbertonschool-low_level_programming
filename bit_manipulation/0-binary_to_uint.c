7f55f3e25845258f5e3ee46c000bcf2fc892b4da
Your Name <you@example.com>
2024-12-06 17:05:12 +0100

    bit manipulation

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -1,42 +1,45 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * binary_to_uint - Converts a binary number to an unsigned int.
- * @b: A pointer to a string of '0' and '1' characters.
+ * _strlen_b - Returns length of a string
+ * @s: Pointer to a string
  *
- * Return: The converted number, or 0 if there is one or more chars in
- *         the string b that is not '0' or '1', or if b is NULL.
+ * Return: Length of string
+ * 0 if any char is not 0 or 1
+ */
+unsigned int _strlen_b(const char *s)
+{
+	int i = 0;
+
+	while (*(s + i))
+	{
+		if (*(s + i) != '0' && *(s + i) != '1')
+			return (0);
+		++i;
+	}
+	return (i);
+}
+
+/**
+ * binary_to_uint - Converts a binary number to an unsigned int
+ * @b: Pointer to a string of 0 and 1 chars
+ *
+ * Return: Converted number
+ * 0 if there is one or more chars in the string that is not 0 or 1
+ * 0 if b is NULL
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int result = 0;
+	unsigned int pow;
+	unsigned int n = 0;
+	unsigned int l = 0;
 
-	/* Check if the string is NULL */
-	if (b == NULL)
-		return (0);
-
-	/* Traverse each character in the string */
-	for (int i = 0; b[i] != '\0'; i++) {
-		/* Check if the current character is '0' or '1' */
-		if (b[i] != '0' && b[i] != '1')
-			return (0);
-
-		/* Shift result to the left (multiply by 2) and add the binary digit */
-		result = result * 2 + (b[i] - '0');
+	if (b)
+		l = _strlen_b(b);
+	for (pow = 0; l; --l, ++pow)
+	{
+		if (*(b + l - 1) == '1')
+			n += 1 << pow;
 	}
-
-	return (result);
+	return (n);
 }
-
-/* Test the function */
-int main(void)
-{
-	const char *binary_str = "1101";
-	unsigned int result;
-
-	result = binary_to_uint(binary_str);
-	printf("The converted number is: %u\n", result); /* Expected output: 13 */
-
-	return (0);
-}
-
---
+++ bit_manipulation/2-get_bit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * get_bit - Returns the value of a bit at a given index
+ * @n: Bit being evaluated
+ * @index: Index, starting from 0 of the bit we want
+ *
+ * Return: Value of the bit at index
+ * -1 if error
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index > 63)
+		return (-1);
+	return (n >> index & 1);
+}
