dd66236089c9b333199f8e02861a620dd743c6b0
Your Name <you@example.com>
2024-12-07 12:08:25 +0100

    singly

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,49 +1,28 @@
 #include "lists.h"
+
 /**
- * add_node - adds a new node at the beginning of a list
- *@head: pointer to the head of the list
- *@str: string to be added
+ * add_dnodeint - Adds a new node at the beginning of a dlistint_t list.
+ * @head: A pointer to the head of the dlistint_t list.
+ * @n: The integer for the new node to contain.
  *
- * Return: returns the address to the new element or NULL
- * if failed
+ * Return: If the function fails - NULL.
+ *         Otherwise - the address of the new node.
  */
-/*
-   1. Allocate memory for a new node.
-   2. If the allocation fails, return NULL.
-   3. Copy the string into a new buffer.
-   4. If the string copy fails, free the new node and return NULL.
-   5. Compute the length of the string.
-   6. Set the new node’s next pointer to point to the current head of the list.
-   7. Set the head of the list to point to the new node.
-   8. Return a pointer to the new node.
-   */
-list_t *add_node(list_t **head, const char *str)
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	char *dup;
-	int len;
-	list_t *new;
-	new = malloc(sizeof(list_t));
+	dlistint_t *new;
+
+	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
-	dup = strdup(str);
-	if (dup == NULL)
-	{
-		free(new);
-		return (NULL);
-	}
-	for (len = 0; str[len];)
-		len++;
-	new->str = dup;
-	new->len = len;
+
+	new->n = n;
+	new->prev = NULL;
 	new->next = *head;
+	if (*head != NULL)
+		(*head)->prev = new;
 	*head = new;
+
 	return (new);
 }
-
-
-
-
-
-
-
 
